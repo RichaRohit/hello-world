5820064fa39b364611c90805151e315212259cd1
RohitNayak24 <mr.rohitnayak@gmail.com>
2020-07-27 23:42:53 +0530

    initial commit

---
+++ dblzero.py
@@ -0,0 +1,16 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        i=0;
+        j=0;
+        while(i<len(arr))
+        if(arr[i]!=0):
+            continue
+            elif(arr[i]==0):
+                j=0
+                while(j<len(arr)):
+                    arr[j+1]=arr[j]
+        
+               
---
+++ filereator.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+def create():
+	print(sys.argv)
+	fileName = str (sys.argv[1]) + str(sys.argv[3])
+	open(fileName,"a").close()
+	os.system("subl " + fileName)
+
+	if name__ == "__main__":
+		create()
